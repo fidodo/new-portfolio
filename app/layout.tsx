d5a6fbe4c331b6b41fa3f925253a9e696ea1a5fb
ayokunle ogunfidodo <89479717+fidodo@users.noreply.github.com>
2025-11-06 02:48:32 +0200

    using better snapshot

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Ayokunle Ogunfidodo's - Portfolio",
   description: "Personal portfolio showcasing my projects and skills",
+  openGraph: {
+    images: ["/og-image.png"],
+  },
 };
 
 export default function RootLayout({
